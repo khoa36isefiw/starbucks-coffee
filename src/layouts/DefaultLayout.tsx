30a8b161add8cf2ae0e9c9e5593d8dc7813df669
khoa36isefiw <huynhdangkhoa961617@gmail.com>
2025-01-25 21:37:36 +0700

    feat: add header

--- src/layouts/DefaultLayout.tsx
+++ src/layouts/DefaultLayout.tsx
@@ -1,11 +1,12 @@
-import { Box, Link } from "@mui/material";
+import { Box } from "@mui/material";
 import { Outlet } from "react-router-dom";
+import Header from "../components/Header";
 
 function DefaultLayout() {
   return (
     <Box>
       {/* Header */}
-      <Box>Header</Box>
+      <Header />
 
       <main>
         <Outlet />
