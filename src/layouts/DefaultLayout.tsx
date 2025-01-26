f876ae9fc00cb5f8561206d4c212f457f1cecb50
khoa36isefiw <huynhdangkhoa961617@gmail.com>
2025-01-26 22:10:20 +0700

    style: re-make header ui

--- src/layouts/DefaultLayout.tsx
+++ src/layouts/DefaultLayout.tsx
@@ -1,19 +1,19 @@
-import { Box } from "@mui/material";
-import { Outlet } from "react-router-dom";
-import Header from "../components/Header";
+import { Box } from '@mui/material';
+import { Outlet } from 'react-router-dom';
+import Header from '../components/Header';
 
 function DefaultLayout() {
-  return (
-    <Box>
-      {/* Header */}
-      <Header />
+    return (
+        <Box>
+            {/* Header */}
+            <Header />
 
-      <main>
-        <Outlet />
-      </main>
-      <footer>ahihihi</footer>
-    </Box>
-  );
+            <main>
+                <Outlet />
+            </main>
+            <footer>ahihihi</footer>
+        </Box>
+    );
 }
 
 export default DefaultLayout;
