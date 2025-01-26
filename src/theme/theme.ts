400e04227a26f205ff890048f85f67eb7fd37c0f
khoa36isefiw <huynhdangkhoa961617@gmail.com>
2025-01-26 22:09:35 +0700

    style: add theme for styling

--- src/theme/theme.ts
+++ src/theme/theme.ts
@@ -1,12 +1,29 @@
-
 import { createTheme } from "@mui/material";
+import { Theme, ThemeOptions } from '@mui/material/styles';
+
+declare module '@mui/material/styles' {
+    interface CustomTheme extends Theme {
+        text: {
+            textBlack: string;
+        };
+    }
+    // allow configuration using `createTheme`
+    interface CustomThemeOptions extends ThemeOptions {
+        text?: {
+            textBlack?: string;
+        };
+    }
+    export function createTheme(options?: CustomThemeOptions): CustomTheme;
+}
 
 export const theme = createTheme({
     palette: {
         primary: {
             main: '#00754a',
-
         }
+    },
+    text: {
+        textBlack: '#000000de',
     }
 });
 
