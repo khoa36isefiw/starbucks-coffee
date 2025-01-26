7f176ab56a61cc0540e0578b360635ad76132f14
khoa36isefiw <huynhdangkhoa961617@gmail.com>
2025-01-26 22:52:17 +0700

    refactor: layout of theme color

--- src/theme/theme.ts
+++ src/theme/theme.ts
@@ -3,14 +3,16 @@ import { Theme, ThemeOptions } from '@mui/material/styles';
 
 declare module '@mui/material/styles' {
     interface CustomTheme extends Theme {
-        text: {
+        color: {
             textBlack: string;
+            hoverButtonColor: string;
         };
     }
     // allow configuration using `createTheme`
     interface CustomThemeOptions extends ThemeOptions {
-        text?: {
+        color?: {
             textBlack?: string;
+            hoverButtonColor?: string;
         };
     }
     export function createTheme(options?: CustomThemeOptions): CustomTheme;
@@ -22,8 +24,9 @@ export const theme = createTheme({
             main: '#00754a',
         }
     },
-    text: {
+    color: {
         textBlack: '#000000de',
+        hoverButtonColor: '#e5e5e5'
     }
 });
 
