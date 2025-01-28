a776cfaf0dda96d395fe18f00461c451f4046084
khoa36isefiw <huynhdangkhoa961617@gmail.com>
2025-01-28 22:44:31 +0700

    style: make active header

--- src/layouts/DefaultLayout.tsx
+++ src/layouts/DefaultLayout.tsx
@@ -1,14 +1,12 @@
 import { Box } from '@mui/material';
 import { Outlet } from 'react-router-dom';
 import Header from '../components/Header';
-import HeaderV2 from '../components/HeaderV2';
 
 function DefaultLayout() {
     return (
         <Box>
             {/* Header */}
-            {/* <Header /> */}
-            <HeaderV2 />
+            <Header />
 
             <main>
                 <Outlet />
