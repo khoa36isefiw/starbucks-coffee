b3d4c082b3d73a44a3effc355123cb913d18c6f8
khoa36isefiw <huynhdangkhoa961617@gmail.com>
2025-02-01 09:57:36 +0700

    chore: data for footer

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -15,3 +15,56 @@ export const HEADER_DATA: {
             link: '/gift-cards',
         },
     ];
+
+
+export const about: string[] = [
+    "Our Company",
+    "Our Coffee",
+    "About Starbucks",
+    "Starbucks Archive",
+    "Investor Relations",
+    "Customer Service",
+    "Contact Us"
+]
+
+export const careers: string[] = [
+    "Culture and Values",
+    "Inclusion, Diversity, & Belonging",
+    "College Achievement Plan",
+    "Alumni Community",
+    "U.S. Careers",
+    "International Careers",
+];
+export const social: string[] = [
+    "Communities",
+    "Starbucks Foundation",
+    "Sustainability",
+    "Environmental and Social Impact Reporting",
+];
+export const businessPartners: string[] = [
+    "Landlord Support Center",
+    "Suppliers",
+    "Corporate Gift Card Sales",
+    "Office and Foodservice Coffee",
+
+];
+export const orderPickup: string[] = [
+    "Order on the App",
+    "Order on the Web",
+    "Delivery",
+    "Order and Pick Up Options",
+    "Explore and Find Coffee for Home",
+];
+
+export const footerData: string[] = [
+    "Consumer Health Privacy Notice",
+    "Terms of Use",
+    "Do Not Share My Personal Information",
+    "CA Supply Chain Act",
+    "Accessibility",
+    "Cookie Preferences",
+]
+
+
+
+
