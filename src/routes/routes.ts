b5b6af6ca7602e38a0ca6f76a9ede24b1f5a881c
khoa36isefiw <huynhdangkhoa961617@gmail.com>
2025-02-04 23:05:24 +0700

    feat: add routes for menu page

--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -1,6 +1,9 @@
+
 import HomePage from "../pages/HomePage";
+import Menu from "../pages/Menu";
 
 export const publicRoutes = [
     { path: '/', component: HomePage },
+    { path: '/menu', component: Menu },
 ];
 export const privateRoutes = [];
