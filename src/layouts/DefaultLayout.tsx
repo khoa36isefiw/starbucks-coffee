6fe74af1fff994d68264aa00a501bc74a9b04e09
khoa36isefiw <huynhdangkhoa961617@gmail.com>
2025-02-17 14:02:32 +0700

    feat: handle logic for showing menu header

--- src/layouts/DefaultLayout.tsx
+++ src/layouts/DefaultLayout.tsx
@@ -1,13 +1,19 @@
 import { Box } from '@mui/material';
-import { Outlet } from 'react-router-dom';
+import { Outlet, useLocation } from 'react-router-dom';
 import Header from '../components/Header/Header';
 import Footer from '../components/Footer/Footer';
+import MenuHeader from '../components/Header/MenuHeader';
 
 function DefaultLayout() {
+    const location = useLocation();
+    // only show if path name includes /menu and not include /menu/feature
+    const showMenuHeader =
+        location.pathname.includes('/menu') && !location.pathname.includes('/menu/feature');
     return (
         <Box sx={{ minHeight: '150vh' }}>
             {/* Header */}
             <Header />
+            {showMenuHeader && <MenuHeader />}
 
             <Box>
                 <Outlet />
--- src/pages/Menu.tsx
+++ src/pages/Menu.tsx
@@ -6,7 +6,7 @@ import MenuContent from '../components/CoffeeMenu/MenuContent';
 function Menu() {
     return (
         <Box sx={{ minHeight: '150vh', overflow: 'visible', display: 'block' }}>
-            <MenuHeader />
+            {/* <MenuHeader /> */}
             <MenuContent />
         </Box>
     );
