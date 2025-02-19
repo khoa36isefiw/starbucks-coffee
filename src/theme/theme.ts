6ffeb8c3288295ae06a2b36840330dcc914ab4e2
khoa36isefiw <huynhdangkhoa961617@gmail.com>
2025-02-19 13:38:07 +0700

    style: add theme color

--- src/theme/theme.ts
+++ src/theme/theme.ts
@@ -13,7 +13,10 @@ declare module '@mui/material/styles' {
             shadowColor: string;
             hoverIconFooter: string;
             rewardColor?: string;
-            rewardTextColor: string;
+            rewardTextColor?: string;
+            rewardTabBg?: string;
+            rewardContainer?: string;
+
         };
     }
     // allow configuration using `createTheme`
@@ -27,7 +30,9 @@ declare module '@mui/material/styles' {
             shadowColor?: string;
             hoverIconFooter?: string;
             rewardColor?: string;
-            rewardTextColor: string;
+            rewardTextColor?: string;
+            rewardTabBg?: string;
+            rewardContainer?: string;
 
         };
     }
@@ -50,6 +55,8 @@ export const theme = createTheme({
         hoverIconFooter: '#C2C0C0FF',
         rewardColor: '#188e5a',
         rewardTextColor: '#036140',
+        rewardTabBg: '#f1f7f5',
+        rewardContainer: '#d4e9e2',
     }
 });
 
