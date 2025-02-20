ac593da6cb4813a6766690e3391c4830dc280ee8
khoa36isefiw <huynhdangkhoa961617@gmail.com>
2025-02-21 00:00:14 +0700

    feat: customize hooks for opening the modal

---
+++ src/hooks/useOpenModal.tsx
@@ -0,0 +1,13 @@
+import React, { useState } from 'react';
+
+export const useOpenModal = () => {
+    const [open, setOpen] = useState(false);
+    const handleOpen = () => setOpen(true);
+    const handleClose = () => setOpen(false);
+
+    return {
+        open,
+        handleOpen,
+        handleClose,
+    };
+};
