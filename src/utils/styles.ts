ad259f550a0b269d5f67c8fb9abbee41b327f34b
khoa36isefiw <huynhdangkhoa961617@gmail.com>
2025-02-22 23:49:42 +0700

    feat: style for flex

---
+++ src/utils/styles.ts
@@ -0,0 +1,13 @@
+export const flexColCenterStyle = {
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+    justifyContent: 'center',
+};
+
+
+export const flexRowCenterStyle = {
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+};
