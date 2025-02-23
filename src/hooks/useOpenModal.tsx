1b62ba6770873bf0a45a5aaa92fa4a1d4c1899cf
khoa36isefiw <huynhdangkhoa961617@gmail.com>
2025-02-23 20:31:55 +0700

    feat: hooks for opening modal

--- src/hooks/useOpenModal.tsx
+++ src/hooks/useOpenModal.tsx
@@ -2,12 +2,17 @@ import React, { useState } from 'react';
 
 export const useOpenModal = () => {
     const [open, setOpen] = useState(false);
-    const handleOpen = () => setOpen(true);
+    const [index, setIndex] = useState(0);
+    const handleOpen = (index: number) => {
+        setOpen(true);
+        setIndex(index);
+    };
     const handleClose = () => setOpen(false);
 
     return {
         open,
         handleOpen,
         handleClose,
+        index,
     };
 };
