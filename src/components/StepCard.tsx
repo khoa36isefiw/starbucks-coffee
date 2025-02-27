31acef1d8c7dd5925433076c6ff8e75a78a9ae77
khoa36isefiw <huynhdangkhoa961617@gmail.com>
2025-02-27 21:54:24 +0700

    style: responsive for rewards page

--- src/components/StepCard.tsx
+++ src/components/StepCard.tsx
@@ -1,4 +1,39 @@
-import { Box, Typography } from '@mui/material';
+// import { Box, Typography } from '@mui/material';
+// import React from 'react';
+// import { theme } from '../theme/theme';
+
+// function StepCard({
+//     image = 'https://www.starbucks.com/weblx/images/rewards/ways-to-pay/2A.png',
+//     content = 'To save time and earn Stars twice as fast, add money to your digital Starbucks Card using any payment option. Scan and pay in one step or order ahead in the app.',
+//     title = 'Preload',
+// }) {
+//     return (
+//         <Box
+//             sx={{
+//                 display: 'flex',
+//                 alignItems: 'flex-start',
+//                 gap: 4,
+//                 maxWidth: '500px',
+
+//                 [theme.breakpoints.down('sm')]: {
+//                     width: '100%',
+//                     mt: 2,
+//                     gap: 2,
+//                 },
+//             }}
+//         >
+//             <img src={image} alt={title} style={{ width: 'auto', height: '160px' }} />
+//             <Box>
+//                 <Typography sx={{ fontSize: '20px', fontWeight: 'bold' }}>{title}</Typography>
+//                 <Typography sx={{ mt: 2 }}>{content}</Typography>
+//             </Box>
+//         </Box>
+//     );
+// }
+
+// export default StepCard;
+
+import { Box, Grid2, Typography } from '@mui/material';
 import React from 'react';
 import { theme } from '../theme/theme';
 
@@ -8,12 +43,14 @@ function StepCard({
     title = 'Preload',
 }) {
     return (
-        <Box
+        <Grid2
+            size={{ xs: 12, sm: 6 }}
             sx={{
                 display: 'flex',
                 alignItems: 'flex-start',
                 gap: 4,
                 maxWidth: '500px',
+
                 [theme.breakpoints.down('sm')]: {
                     width: '100%',
                     mt: 2,
@@ -26,7 +63,7 @@ function StepCard({
                 <Typography sx={{ fontSize: '20px', fontWeight: 'bold' }}>{title}</Typography>
                 <Typography sx={{ mt: 2 }}>{content}</Typography>
             </Box>
-        </Box>
+        </Grid2>
     );
 }
 
