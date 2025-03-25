4ab3da8b5556a72b5d155e26f63d5c2076540f1a
khoa36isefiw <huynhdangkhoa961617@gmail.com>
2025-03-25 23:31:28 +0700

    style: create modal for creating new menu

--- src/components/ui/AdminMenu/AdminCreateMenu.tsx
+++ src/components/ui/AdminMenu/AdminCreateMenu.tsx
@@ -1,5 +1,58 @@
+import { Box, Button, TextField, Typography } from '@mui/material';
+import ModalLayout from './ModalLayout';
+import { useState } from 'react';
+import { ICreateMenu } from '../../../interfaces/IMenu';
+import { TButton } from '../Button/TButton';
+
 function AdminCreateMenu() {
-    return <div>AdminCreateMenu</div>;
+    const [formData, setFormData] = useState<ICreateMenu>({
+        name: '',
+    });
+
+    const handleChange = (
+        e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>,
+        field: string,
+    ) => {
+        setFormData({
+            ...formData,
+            [field]: e.target.value, // get name of text field and it value
+        });
+    };
+
+    const handleSubmit = () => {
+        console.log('formData: ', formData);
+    };
+    return (
+        <ModalLayout>
+            <Box sx={{ display: 'flex', flexDirection: 'column', gap: 2, width: 300 }}>
+                <Typography sx={{ fontSize: '18px', fontWeight: 'bold' }}>
+                    Create new menu
+                </Typography>
+                <TextField
+                    label="Name"
+                    value={formData.name}
+                    onChange={(e) => handleChange(e, 'name')}
+                    variant="outlined"
+                    required
+                />
+
+                <TButton
+                    variant="contained"
+                    onClick={handleSubmit}
+                    sx={{
+                        color: '#fff',
+                        '&:hover': {
+                            color: '#fff',
+                            fontWeight: 'bold',
+                            cursor: 'pointer',
+                        },
+                    }}
+                >
+                    Submit
+                </TButton>
+            </Box>
+        </ModalLayout>
+    );
 }
 
 export default AdminCreateMenu;
